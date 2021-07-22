ead403769de3f5cdd83e948bf022f78d3c37a935
vacantClaudia <vacantclaudia88@gmail.com>
2021-07-22 09:59:53 +0200

    demarrage du projet

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default App;
