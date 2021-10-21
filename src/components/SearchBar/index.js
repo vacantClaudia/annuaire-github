ddfa92616666f7cb9a4662534fae2105d7f3258d
vacantClaudia <vacantclaudia88@gmail.com>
2021-10-21 15:29:09 +0200

    grave à useRef on met un focus sur l input dès le premier rendu

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Input, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-const SearchBar = ({ inputSearch, setInputSearch, handleSubmit }) => (
-  <div>
-    <h1 className="title"><Icon name="github" size="large" />Github</h1>
-    <form onSubmit={handleSubmit}>
-      <Input icon placeholder="Search..." iconPosition="left" className="header-input">
-        <input
-          size="large"
-          type="text"
-          className="header-input"
-          onChange={(event) => {
-            setInputSearch(event.currentTarget.value);
-          }}
-          value={inputSearch}
-        />
-        <Icon name="search" />
-      </Input>
-    </form>
+const SearchBar = ({ inputSearch, setInputSearch, handleSubmit }) => { 
 
-  </div>
-);
+  const textInput = useRef(null);
+
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
+
+  return( 
+    <div>
+      <h1 className="title"><Icon name="github" size="large" />Github</h1>
+      <form onSubmit={handleSubmit}>
+        <Input icon placeholder="Search..." iconPosition="left" className="header-input">
+          <input
+            size="large"
+            type="text"
+            className="header-input"
+            onChange={(event) => {
+              setInputSearch(event.currentTarget.value);
+            }}
+            value={inputSearch}
+            ref={textInput}
+          />
+          <Icon name="search" />
+        </Input>
+      </form>
+
+    </div>
+  )
+};
 SearchBar.propTypes = {
   inputSearch: PropTypes.string.isRequired,
   setInputSearch: PropTypes.func.isRequired,
